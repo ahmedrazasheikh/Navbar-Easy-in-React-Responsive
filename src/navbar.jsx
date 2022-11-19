1fb20c4f70f99f2480837a15ff6c001fbf86f6dc
Ahmed Raza <bhaia9036@gmail.com>
2022-11-19 00:38:11 -0800

    initial commit

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -2,20 +2,29 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import "./App.css"
 const Navbar = () => {
+  
+ 
+ function toggleBar () {
+   let icon =  document.getElementById("kk")
+   icon.classList.toggle("close")
+  const navLinks = document.querySelector(".nav-links");
+  navLinks.classList.toggle('mobile-menu')
+ }
+ 
   return (
     <div>
-      
-      <nav className="navbar">
-  <span>Logo</span>
+    <nav className="navbar">
+  <span>Ahmed Raza</span>
   <div className="nav-links">
     <ul className="nav-menu">
-      <li className="active"> <Link to='/' >Home</Link>  </li>
+    <li className="active"> <Link to='/' >Home</Link>  </li>
       <li><Link to={'/Contact'} >Contact</Link></li>
       <li><Link to={'/Menu'} >Menu</Link></li>
       <li><Link to={'/About'} >About-us</Link></li>
     </ul>
   </div>
-    <i className='bx bx-grid-alt menu-hamburger'></i>
+
+    <img  id='kk' onClick={toggleBar}   className='bx bx-grid-alt menu-hamburger' src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/2048px-Hamburger_icon.svg.png" alt="" />
 
 </nav>
 <header className="header"></header>
